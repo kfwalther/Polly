f306663404021900e38994a78e7ac443d98b4a9c
Kevin <kfwalther@gmail.com>
2023-03-05 18:31:09 -0500

    Adding a txn summary table on our txn page

--- ui/src/components/TransactionsPage.js
+++ ui/src/components/TransactionsPage.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { TransactionsTable } from "./TransactionsTable";
+import { toPercent } from "./Helpers";
 
 // Fetch the transaction data from the server, and return/render the transactions page.
 export default function TransactionsPage() {
@@ -17,6 +18,25 @@ export default function TransactionsPage() {
 
     return (
         <>
+            {/* Display a rollup/summary of all our transactions. */}
+            <table className="txn-summary-table">
+                <th className="txn-summary-table-header">
+                    Transaction Count
+                </th>
+                <th className="txn-summary-table-header">
+                    Win Rate
+                </th>
+                <th className="txn-summary-table-header">
+                    Beat Rate
+                </th>
+                <tbody>
+                    <tr>
+                        <td className="txn-summary-table-cell">{txnList.length}</td>
+                        <td className="txn-summary-table-cell">{toPercent((txnList.filter(x => x.totalReturn > 0).length / txnList.length) * 100.0)}</td>
+                        <td className="txn-summary-table-cell">{toPercent((txnList.filter(x => x.excessReturn > 0).length / txnList.length) * 100.0)}</td>
+                    </tr>
+                </tbody>
+            </table>
             {/* Display all the transactions in a sortable table. */}
             <TransactionsTable
                 txnData={txnList.filter(t => (t.action === "Buy" || t.action === "Sell"))}
