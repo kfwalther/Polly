89343f239f6c598f913aaaa4392c4f905daea4f3
Kevin <kfwalther@gmail.com>
2023-03-07 08:24:58 -0500

    Added chart range filter bar for txn chart, needs formatting though

--- ui/src/components/StockLineChart.js
+++ ui/src/components/StockLineChart.js
@@ -7,7 +7,7 @@ export default function StockLineChart({ chartData }) {
         var data = []
         if ((chartData != null) && (chartData.sp500 != null)) {
             // Put the sorted values in an array, and add a column header.
-            data = chartData.sp500.date.map((k, i) => [k, chartData.sp500.close[i]])
+            data = chartData.sp500.date.map((k, i) => [new Date(k), chartData.sp500.close[i]])
             data.unshift(['Date', 'Price'])
         }
         return data
@@ -19,17 +19,45 @@ export default function StockLineChart({ chartData }) {
         backgroundColor: 'transparent',
         title: "S&P500 Performance",
         curveType: "function",
-        chartArea: { top: 0, bottom: 0, left: 25, right: 25 }
+        chartArea: { top: 25, bottom: 50, left: 50, right: 25 },
+        responsive: true,
     }
     var data = getFilteredChartData()
 
     return (
+        // <div className="txnchart-container">
         <Chart
             chartType="LineChart"
-            width="80%"
+            width="100%"
             height="600px"
             data={data}
             options={chartOptions}
+            controls={[
+                {
+                    controlType: "ChartRangeFilter",
+                    options: {
+                        filterColumnIndex: 0,
+                        ui: {
+                            chartType: "LineChart",
+                            chartOptions: {
+                                backgroundColor: "black",
+                                chartArea: { width: "95%", height: "90%" },
+                                hAxis: { baselineColor: "none" },
+                            },
+                        },
+                    },
+                    controlPosition: "bottom",
+                    controlWrapperParams: {
+                        state: {
+                            range: {
+                                start: new Date(2020, 1, 1),
+                                end: new Date(2023, 2, 28),
+                            },
+                        },
+                    },
+                },
+            ]}
         />
+        // </div>
     );
 }
